09e4e93f01bc20f3ea7a20c6499038eeae2fea35
Ernesto Level <ealevel@medianoche.local>
2023-12-01 23:28:29 -0500

    AOC 2023, day 1

--- 2023/day1.py
+++ 2023/day1.py
@@ -16,11 +16,6 @@ def extract_digits(input_string):
   matches = re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', input_string)
   first_digit = map[matches[0]] if matches[0] in map else int(matches[0])
   last_digit = map[matches[-1]] if matches[-1] in map else int(matches[-1])
-  print(input_string, " -> ", matches[0], matches[-1], 10 * first_digit + last_digit)
   return 10 * first_digit + last_digit
 
-s = 0
-for line in sys.stdin:
-  str = line.strip()
-  s += extract_digits(str)
-print(s)
+print(sum(extract_digits(line.strip()) for line in sys.stdin))
