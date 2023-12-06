a1e0b34403564bd8abcb7514981094a22bafde4f
Ernesto Level <ealevel@medianoche.local>
2023-12-06 01:11:04 -0500

    AOC 2023, day 6

--- 2023/day6.py
+++ 2023/day6.py
@@ -1,15 +1,6 @@
 import sys
 import math
 
-line1 = sys.stdin.readline().strip().split(':')[1].strip()
-line2 = sys.stdin.readline().strip().split(':')[1].strip()
-
-time = [int(t) for t in line1.split()]
-dist = [int(t) for t in line2.split()]
-
-time2 = int(line1.replace(' ', ''))
-dist2 = int(line2.replace(' ', ''))
-
 # Solve s in: s*t - s^2 > d => s^2 - t*s + d < 0
 # Return numbers between floor(highest) - ceil(lowest) + 1
 # That is, solutions that will produce longer distance than d.
@@ -18,9 +9,16 @@ def count(t, d):
     hi = (t + (t**2 - 4*d) ** 0.5) / 2
     return math.floor(hi) - math.ceil(lo) + 1
 
+line1 = sys.stdin.readline().strip().split(':')[1].strip()
+line2 = sys.stdin.readline().strip().split(':')[1].strip()
+
+time = [int(t) for t in line1.split()]
+dist = [int(t) for t in line2.split()]
 res = 1
 for t in range(len(time)):
     res *= count(time[t], dist[t])
 print(res)
 
+time2 = int(line1.replace(' ', ''))
+dist2 = int(line2.replace(' ', ''))
 print(count(time2, dist2))
