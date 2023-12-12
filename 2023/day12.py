8317511e83bd6527a8290b18c287d3acac35733d
Ernesto Level <ealevel@gmail.com>
2023-12-12 02:22:25 -0500

    AOC 2023, day 12

--- 2023/day12.py
+++ 2023/day12.py
@@ -6,33 +6,26 @@ def count(row, damaged, s_idx, d_idx, mem):
   if s_idx >= len(row):
     # no more springs, only valid if matched all damaged
     return d_idx == len(damaged)
+  if d_idx == len(damaged):
+    # only valid (and 1) if there's no damaged in remaining springs
+    return '#' not in row[s_idx:]
     
   if (s_idx, d_idx) in mem:
     return mem[(s_idx,d_idx)]
 
   cnt = 0
   if row[s_idx] == '?':
-    if d_idx == len(damaged):
-      # can only be operational
-      return count(row, damaged, s_idx+1, d_idx, mem)
     if is_valid(row, s_idx, damaged[d_idx]):
-      # assume this spring is damaged
+      # next damaged[d_idx] can be damaged
       cnt += count(row, damaged, s_idx+damaged[d_idx]+1, d_idx+1, mem)
-      # assume this spring is operational
-      cnt += count(row, damaged, s_idx+1, d_idx, mem)
-    else:
-      # can only be operational
-      return count(row, damaged, s_idx+1, d_idx, mem)
+    # also count as if this spring is operational
+    cnt += count(row, damaged, s_idx+1, d_idx, mem)
   if row[s_idx] == '#':
-    if d_idx == len(damaged):
-      # invalid: there's no more damaged to match
-      return 0
     if is_valid(row, s_idx, damaged[d_idx]):
-      # note that needs to skip the spring after the last one
+      # next damaged[d_idx] can be damaged
       cnt += count(row, damaged, s_idx+damaged[d_idx]+1, d_idx+1, mem)
     else:
-      # invalid: this is not a possible combination to match
-      return 0
+      return 0 # invalid
   mem[(s_idx,d_idx)] = cnt
   return cnt
 
